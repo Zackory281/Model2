95f578efecd712d77bce0bb5fef4809bd8647bad
Zijin Cui <zijincui281@berkeley.edu>
2018-11-29 15:50:49 -0800

    Initial Commit

---
+++ Model2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Model2
+//
+//  Created by Zackory Cramer on 11/29/18.
+//  Copyright © 2018 Zackori Cui. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
