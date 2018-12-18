d5452d8092a2601af20a098f781ba99f4ee0e7a6
Zijin Cui <zijincui281@berkeley.edu>
2018-12-17 16:20:11 -0800

    Evolution system update.

--- ModelCommand/TickBase test/TestBaseTest.swift
+++ ModelCommand/TickBase test/TestBaseTest.swift
@@ -8,26 +8,3 @@
 
 import Foundation
 
-class O: Tickable {
-	func tick(_ tick: Tick) -> Bool {
-		print("\(priority) ticked")
-		return true
-	}
-	
-	var priority: TickPri
-	
-	init(_ i: TickPri) {
-		priority = i
-	}
-}
-
-let tb = TickBase()
-
-func runTBT() {
-	tb.add(tickable: O(0))
-	tb.add(tickable: O(3))
-	tb.add(tickable: O(2))
-	tb.add(tickable: O(53))
-	tb.add(tickable: O(1))
-	print(tb.reapTick())
-}
